716fb380edfa4ebf5055f17712ddfda391914961
Danny Neel <dneel10@gmail.com>
2022-12-30 23:44:14 -0500

    add test for valid_entry

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+
 class Player
-  attr_reader :color, :selected_piece
+  attr_reader :color
+  attr_accessor :selected_cell
+
   def initialize
     @color = color
-    @selected_piece = []
+    @selected_cell = nil
   end
 
   def player_turn
+    # select a cell on the board with a piece on it
+    select_cell
+    # select cell to move the piece to
+
   end
 
-  def select_piece
-    user_input = gets.chomp
-    selected_piece << user_input
+  # reuse this for any time you need to select a cell (picking a piece, or selecting
+  # the new space)
+  def select_cell
+    loop do
+      user_input = gets.chomp
+      return @selected_cell = user_input if valid_entry?(user_input)
+
+      puts "Invalid Selection. Please select a valid cell"
+    end
+  end
+
+  def valid_entry?(input)
+    input.match?(/[a-hA-H][1-8]/)
   end
 end
