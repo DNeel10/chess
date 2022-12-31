8180825ef2013af5a48c6073ddb736e5c2742d63
Danny Neel <dneel10@gmail.com>
2022-12-31 18:47:36 -0500

    add methods to find valid moves for the knight and pass tests

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,3 +1,4 @@
+
 class Knight
   attr_reader :move_pattern, :position, :color
 
@@ -9,7 +10,20 @@ class Knight
                      [2, 1], [2, -1], [-2, 1], [-2, -1]]
   end
 
-  def find_moves
+  def valid_moves(board)
+    valid_moves_list = []
+    initial_move_list = potential_moves
+    initial_move_list.each do |move|
+      if board[move[0]][move[1]].nil?
+        valid_moves_list << move
+      else
+        valid_moves_list << move unless board[move[0]][move[1]].color == @color
+      end
+    end
+    valid_moves_list
+  end
+
+  def potential_moves
     move_pattern.map { |x, y| [position[0] + x, position[1] + y] }
                 .select { |move| move.all? { |n| n >= 0 && n <= 7 } }
   end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,22 +1,44 @@
 require './lib/knight'
+require './lib/pawn'
 
 describe Knight do
-  describe "#find_moves" do
-    context "an unmoved knight is selected by the player" do
+  describe '#potential_moves' do
+    context 'an unmoved knight is selected by the player' do
       subject(:knight_moves) { described_class.new([0,1], 'Black') }
 
-      it "returns an array of all move options" do
-        move_array = knight_moves.find_moves
+      it 'returns an array of all move options' do
+        move_array = knight_moves.potential_moves
         expect(move_array).to eq([[1, 3], [2, 2], [2, 0]])
       end
     end
 
-    context "a knight in a subsequent position is selected by the player" do
+    context 'a knight in a subsequent position is selected by the player' do
       subject(:knight_subsequent) { described_class.new([4, 5], 'Black') }
 
-      it "returns an array of all move options" do
-        move_array = knight_subsequent.find_moves
+      it 'returns an array of all move options' do
+        move_array = knight_subsequent.potential_moves
         expect(move_array).to eq([[5, 7], [5, 3], [3, 7], [3, 3], [6, 6], [6, 4], [2, 6], [2, 4]])
+      end
+    end
+  end
+
+  describe '#valid_moves' do
+    context 'a players potential moves may be limited by his own pieces' do
+      subject(:knight_restricted) { described_class.new([0, 1], 'Black') }
+      let(:player_piece) { instance_double(Pawn) }
+
+      it 'removes moves where a players own piece is currently occupying' do
+        board = Array.new(3) { Array.new(3, nil) }
+        board[1][3] = Pawn.new([1, 3], 'Black')
+        valid_list = knight_restricted.valid_moves(board)
+        expect(valid_list).to eq([[2, 2], [2, 0]])
+      end
+
+      it 'does not remove a move where an enemy players piece is currently occupying' do
+        board = Array.new(3) { Array.new(3, nil) }
+        board[1][3] = Pawn.new([1, 3], 'White')
+        valid_list = knight_restricted.valid_moves(board)
+        expect(valid_list).to eq([[1, 3], [2, 2], [2, 0]])
       end
     end
   end
