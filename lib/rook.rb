22a93144943287c857e38a44b754d48f7e544370
Danny Neel <dneel10@gmail.com>
2023-01-18 23:37:08 -0500

    add rook move logic and tests

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,12 +1,13 @@
 class Rook
   attr_reader :move_pattern, :color
-  attr_accessor :position
+  attr_accessor :position, :moves
 
   # what needs to be set up when a piece is created in the game
   def initialize(position, color)
     @position = position
     @color = color
     @move_pattern = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    @moves = []
   end
 
   def valid_moves(board)
@@ -15,12 +16,70 @@ class Rook
 
   # rook can use move pattern in one (up, down, left, right) direction up to where another piece is 
   # (capturing, or one before)
-  def potential_moves
-
+  def potential_moves(board)
+    move_right(board)
+    move_left(board)
+    move_up(board)
+    move_down(board)
   end
 
   def legal_move?(board, coordinates)
     valid_moves(board).include?(coordinates)
   end
 
+  def move_right(board, moves = @moves, position = @position)
+    queue = [position]
+
+    while queue
+      current = queue.shift
+
+      return if current[1] >= 7 || board.players_piece?(current, color)
+
+      new_move = [current[0], current[1] + 1]
+      queue << new_move
+      moves << new_move if board.open_space?(new_move) || board.opponent_piece?(new_move)
+    end
+  end
+
+  def move_left(board, moves = @moves, position = @position)
+    queue = [position]
+
+    while queue
+      current = queue.shift
+
+      return if current[1] <= 0 || board.players_piece?(current, color)
+
+      new_move = [current[0], current[1] - 1]
+      queue << new_move
+      moves << new_move if board.open_space?(new_move) || board.opponent_piece?(new_move)
+    end
+  end
+
+  def move_up(board, moves = @moves, position = @position)
+    queue = [position]
+
+    while queue
+      current = queue.shift
+
+      return if current[0] >= 7 || board.players_piece?(current, color)
+
+      new_move = [current[0] + 1, current[1]]
+      queue << new_move
+      moves << new_move if board.open_space?(new_move) || board.opponent_piece?(new_move)
+    end
+  end
+
+  def move_down(board, moves = @moves, position = @position)
+    queue = [position]
+
+    while queue
+      current = queue.shift
+
+      return if current[0] <= 0 || board.players_piece?(current, color)
+
+      new_move = [current[0] - 1, current[1]]
+      queue << new_move
+      moves << new_move if board.open_space?(new_move) || board.opponent_piece?(new_move)
+    end
+  end
 end
---
+++ spec/rook_spec.rb
@@ -0,0 +1,38 @@
+require './lib/rook'
+require './lib/board'
+
+describe Rook do
+  describe '#potential_moves' do
+    subject(:rook_move) { described_class.new([0, 0], 'White') }
+    let(:board_grid) { Board.new }
+
+    before do
+      allow(board_grid).to receive(:open_space?).and_return true
+      allow(board_grid).to receive(:players_piece?).and_return false
+    end
+
+    it 'returns an array of moves in each direction' do
+      move_array = rook_move.instance_variable_get(:@moves)
+      rook_move.potential_moves(board_grid)
+      expect(move_array).to eq([[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+                               [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]])
+    end
+  end
+
+  describe '#move_right' do
+    subject(:rook_right) { described_class.new([0, 0], 'White') }
+    let(:board_grid) { Board.new }
+
+    before do
+      allow(board_grid).to receive(:open_space?).and_return true
+      allow(board_grid).to receive(:players_piece?).and_return false
+    end
+
+    it 'returns an array of moves in the horizontal right direction' do
+      move_right_array = rook_right.instance_variable_get(:@moves)
+      rook_right.move_right(board_grid)
+      expect(move_right_array).to eq([[0, 1], [0, 2], [0, 3], [0, 4],
+                                      [0, 5], [0, 6], [0, 7]])
+    end
+  end
+end
