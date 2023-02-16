4fb91880ab9f942b2da8a206cdc9e98cc40ac8e7
Daniel Neel <dneel10@gmail.com>
2023-02-15 23:36:02 -0500

    begin adding checkmate methods

--- lib/checkmate.rb
+++ lib/checkmate.rb
@@ -1,13 +1,33 @@
 class Checkmate
 
-  def initialize
+  def initialize(board)
+    @board = board
   end
   
   def checkmate?
+    if king.moves.empty?
+      return true if blocking_moves.empty?
+
+    end
+    false
   end
 
-  def blocking_moves
+  def blocking_moves(king)
+    move_list = []
+
+    move_list
+  
     # opponent piece coordinates Ox, Oy
     # king position Coordinates Kx, Ky
     # All player pieces check for moves that are between (Oy, Py) and (Ox, Px)
+  
+  end
+
+  def opponent_piece(king)
+    board.grid.flatten.compact.each do |piece|
+      next if piece.color == king.color
+
+      piece
+    end
+  end
 end
