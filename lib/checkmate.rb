d8cd1fe0f728c76ed834063dc989af4159d6f463
Danny Neel <dneel10@gmail.com>
2023-02-15 23:24:20 -0500

    remove lint issue

--- lib/checkmate.rb
+++ lib/checkmate.rb
@@ -10,4 +10,5 @@ class Checkmate
     # opponent piece coordinates Ox, Oy
     # king position Coordinates Kx, Ky
     # All player pieces check for moves that are between (Oy, Py) and (Ox, Px)
+  end
 end
